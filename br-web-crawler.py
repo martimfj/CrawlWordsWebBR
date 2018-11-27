552c06962bfd705fbe83df2008338f1cfe5ece9c
Leonardo Medeiros <leostayner@gmail.com>
2018-11-27 18:43:51 -0200

    scripts adicionados

---
+++ br-web-crawler.py
@@ -0,0 +1,53 @@
+import math
+import numpy as np
+import pickle
+from scipy.stats import norm
+dummy = 1000
+#Nt = quantidade da palavra no dicionario total
+#Ne = quantidade da palavra no dicionario do estado
+#Pt = fracao da palavra/todas as palavras no dic total
+#Pe = fracao da palavra/todas as palavras no dic do estado
+def calcula_pvalue(nt, ne, pt, pe):
+    P = ((nt*pt)+(ne*pe))/(nt+ne)
+    if pe > pt:
+        Z = (pe-pt)/math.sqrt(P*(1-P)*((1/ne)+(1/nt)))
+    else:
+        Z = (pt-pe)/math.sqrt(P*(1-P)*((1/ne)+(1/nt)))
+    p_value = (1 - norm.cdf(Z))*2
+    return p_value
+
+def get_total(dic_label):
+    return sum(contagem+1000 for contagem in dic_label.values())
+
+def save(data, name):	
+    with open(name, 'wb') as handle:
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+## MAIN ##
+with open('./Pickle/frequencia_palavras_big.pickle', 'rb') as handle:
+    dic_load = pickle.load(handle)
+    for estado, lista_palavras in dic_load.items():
+        dic_load[estado] = dict(lista_palavras)
+
+with open('./Pickle/frequencia_palavras_geral_big.pickle', 'rb') as handle:
+    lista_geral_load = pickle.load(handle)
+    dic_geral_load = dict(lista_geral_load)
+
+total = get_total(dic_geral_load)
+dic_final = {}
+
+for estado, dic_estado in dic_load.items():
+    
+    dic_final[estado] = [] 
+    total_capital  = get_total(dic_estado)
+
+    for palavra, Ne in dic_estado.items():
+        Nt = dic_geral_load[palavra] if palavra in dic_geral_load else 0.0
+        Nt += 1000
+        Ne += 1000
+        Pt = Nt / total
+        Pe = Ne / total_capital
+        pvalue  = calcula_pvalue(Nt, Ne, Pt, Pe)
+        dic_final[estado].append((palavra, Pt, Pe, pvalue))
+
+save(dic_final, "./Pickle/palavras_pvalue.pickle")
